c97d2bd3469af435d8f17754ed29f6e705c87048
40823150 <40823150@gm.nfu.edu.tw>
2021-01-06 17:13:13 +0800

    W2~W4 add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'W1', 'text': '\n', 'tags': '', 'url': 'W1.html'}, {'title': 'W2~W4', 'text': '\n', 'tags': '', 'url': 'W2~W4.html'}]};
+var tipuesearch = {"pages": [{'title': 'W1', 'text': '\n', 'tags': '', 'url': 'W1.html'}, {'title': 'W2~W4', 'text': 'wink 轉影片 教學 \n \n', 'tags': '', 'url': 'W2~W4.html'}]};
